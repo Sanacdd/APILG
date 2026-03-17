379d8ad0808c60ee217a43e8a9f8153640bb2c92
David Sanabria <sanabriadavid112@gmail.com>
2026-03-16 22:41:37 -0600

    maestro update y insert post-put

--- app/Controllers/maestroControllers.js
+++ app/Controllers/maestroControllers.js
@@ -1,6 +1,5 @@
 'use strict'
 
-//const { request } = require('../app');
 const db = require('../config/db');
 const Maestro = db.maestro;  
 
@@ -9,10 +8,53 @@ Maestro.findAll()
     .then(data => {
         res.status(200).send(data);
     })
-    .catch(error =>{;
+    .catch(error =>{
       res.status(400).send(error);
-})
+    })
 }
+
+async function insertMaestro(request, response){
+    const maestroInsert = request.body;
+
+    Maestro.create({
+        maestro_id: maestroInsert.id,
+        first_name: maestroInsert.name,
+        last_name: maestroInsert.last,
+    })
+    .then(data => {
+        response.status(200).send(data);
+    })
+    .catch(error => {
+        response.status(400).send(error);
+    });
+}
+
+async function updateMaestro(request, response){
+    const maestroUpdate = request.body;
+
+    Maestro.update(maestroUpdate, {
+        where: { maestro_id: maestroUpdate.id }
+    })
+    .then(num => {
+        if(num == 1){
+            response.status(200).send({
+                message: "Maestro actualizado correctamente"
+            });
+        } else {
+            response.status(400).send({
+                message: "No se pudo actualizar el maestro"
+            });
+        }
+    })
+    .catch(error => {
+        response.status(500).send({
+            message: error.message || "Error al actualizar el maestro"
+        });
+    });
+}
+
 module.exports = {
-    findAll
+    findAll,
+    insertMaestro,
+    updateMaestro
 }
--- app/routes/maestroRoutes.js
+++ app/routes/maestroRoutes.js
@@ -7,6 +7,8 @@ const apiRoutes = express.Router();
 apiRoutes
 .get('/maestros', maestroController.findAll)
 .get('/getMaestros', async (req, res) => await 
-maestroController.findAll(req, res));
+maestroController.findAll(req, res))
+.post('/insertMaestro', maestroController.insertMaestro)
+.put('/updateMaestro', maestroController.updateMaestro);
 
 module.exports = apiRoutes;
