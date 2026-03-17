5176fc56fa5acafc77d8a585dceee6601d16ab86
cresbrit <carlosalberto240405@gmail.com>
2026-03-16 22:15:32 -0600

    post put clase

--- app/routes/claseRoutes.js
+++ app/routes/claseRoutes.js
@@ -1,11 +1,12 @@
 'use strict'
 const express = require('express');
-const claseController = require('../Controllers/claseController');
-
+const claseController = require('../Controllers/claseController'); 
 const apiRoutes = express.Router();
 
 apiRoutes
 .get('/clases', claseController.findAll)
-.get('/getClases', async (req, res) => await claseController.findAll(req, res));
-
+.get('/getClases', async (req, res) => await 
+claseController.findAll(req, res))
+.post('/insertClase', claseController.insertClase)
+.put('/updateClase', claseController.updateClase);
 module.exports = apiRoutes;
