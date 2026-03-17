a5c0a728d0c883af814553ff260899e7adfcf5eb
cresbrit <carlosalberto240405@gmail.com>
2026-03-16 21:52:18 -0600

    insert clase

--- app/Controllers/claseController.js
+++ app/Controllers/claseController.js
@@ -10,9 +10,50 @@ async function findAll(req, res){
         })
         .catch(error =>{
             res.status(400).send(error);
-        })
+        });
+}
+
+async function insertClase(request, response){
+    const claseInsert = request.body;
+
+    Clase.create({
+        nombre: claseInsert.nombre,
+        descripcion: claseInsert.descripcion,
+    })
+    .then(data => {
+        response.status(200).send(data);
+    })
+    .catch(error => {
+        response.status(400).send(error);
+    });
+}
+
+async function updateClase(request, response){
+    const claseUpdate = request.body;
+
+    Clase.update(claseUpdate, {
+        where: { id: claseUpdate.id }
+    })
+    .then(num => {
+        if(num == 1){
+            response.status(200).send({
+                message: "Clase actualizada correctamente"
+            });
+        } else {
+            response.status(400).send({
+                message: "No se pudo actualizar la clase"
+            });
+        }
+    })
+    .catch(error => {
+        response.status(500).send({
+            message: error.message || "Error al actualizar la clase"
+        });
+    });
 }
 
 module.exports = {
-    findAll
+    findAll,
+    insertClase,
+    updateClase
 }
