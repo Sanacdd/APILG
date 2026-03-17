008060a7ee6a309d3ddf3b779033705d2f1f7710
Jesé Obed Manzanares Cerrato <jesecerrato17@gmail.com>
2026-03-16 22:02:02 -0600

    Resolver conflicto .env

--- app/Controllers/alumnoController.js
+++ app/Controllers/alumnoController.js
@@ -1,20 +1,61 @@
 'use strict'
 
-//const { request } = require('../app');
 const db = require('../config/db');
 const Actor = db.alumno;  
 
-//funcion del control hace traer la data, convertirla
-//llamdo de actor y decir que hacer todo eso
 async function findAll(req, res){
-Actor.findAll()
-    .then(data => {
-        res.status(200).send(data);
-    })
-    .catch(error =>{;
-      res.status(400).send(error);
-})
+    Actor.findAll()
+        .then(data => {
+            res.status(200).send(data);
+        })
+        .catch(error => {
+            res.status(400).send(error);
+        });
 }
+
+async function insertAlumno(request, response){
+    const alumnoInsert = request.body;
+
+    Actor.create({
+        alumno_id: alumnoInsert.id,
+        first_name: alumnoInsert.name,
+        last_name: alumnoInsert.last,
+        // Agrega los demás campos de tu tabla alumno
+    })
+    .then(data => {
+        response.status(200).send(data);
+    })
+    .catch(error => {
+        response.status(400).send(error);
+    });
+}
+
+async function updateAlumno(request, response){
+    const alumnoUpdate = request.body;
+
+    Actor.update(alumnoUpdate, {
+        where: { alumno_id: alumnoUpdate.id }
+    })
+    .then(num => {
+        if(num == 1){
+            response.status(200).send({
+                message: "Alumno actualizado correctamente"
+            });
+        } else {
+            response.status(400).send({
+                message: "No se pudo actualizar el alumno"
+            });
+        }
+    })
+    .catch(error => {
+        response.status(500).send({
+            message: error.message || "Error al actualizar el alumno"
+        });
+    });
+}
+
 module.exports = {
-    findAll
+    findAll,
+    insertAlumno,
+    updateAlumno
 }
