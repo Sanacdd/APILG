c899bfabd9336d085fd91dc4ebe55e1a5b452c4d
Carolina Blandon <carolinamasielblandon@gmail.com>
2026-04-11 23:30:06 -0600

    Se agrega deleteAlumno y corrección de rutas en API

--- app/Controllers/alumnoController.js
+++ app/Controllers/alumnoController.js
@@ -1,63 +1,88 @@
 'use strict'
 
 const db = require('../config/db');
-const Alumno = db.alumno;  
+const Alumno = db.alumno;
 
-async function findAll(req, res){
-    Alumno.findAll()
-        .then(data => {
-            res.status(200).send(data);
-        })
-        .catch(error => {
-            res.status(400).send(error);
-        });
-}
-
-async function insertAlumno(request, response){
-    const alumnoInsert = request.body;
-
-    Alumno.create({
-        ID_Grado: alumnoInsert.ID_Grado,
-        Nombre: alumnoInsert.Nombre,
-        Apellido: alumnoInsert.Apellido,
-        Fecha_Nacimiento: alumnoInsert.Fecha_Nacimiento,
-        Direccion: alumnoInsert.Direccion,
-        Genero: alumnoInsert.Genero
-    })
+async function findAll(req, res) {
+  Alumno.findAll()
     .then(data => {
-        response.status(200).send(data);
+      res.status(200).send(data);
     })
     .catch(error => {
-        response.status(400).send(error);
+      res.status(400).send(error);
     });
 }
 
-async function updateAlumno(request, response){
-    const alumnoUpdate = request.body;
+async function insertAlumno(request, response) {
+  const alumnoInsert = request.body;
 
-    Alumno.update(alumnoUpdate, {
-        where: { ID_Alumno: alumnoUpdate.ID_Alumno }
-    })
-    .then(num => {
-        if(num == 1){
-            response.status(200).send({
-                message: "Alumno actualizado correctamente"
-            });
-        } else {
-            response.status(400).send({
-                message: "No se pudo actualizar el alumno"
-            });
-        }
+  Alumno.create({
+    ID_Grado: alumnoInsert.ID_Grado,
+    Nombre: alumnoInsert.Nombre,
+    Apellido: alumnoInsert.Apellido,
+    Fecha_Nacimiento: alumnoInsert.Fecha_Nacimiento,
+    Direccion: alumnoInsert.Direccion,
+    Genero: alumnoInsert.Genero
+  })
+    .then(data => {
+      response.status(200).send(data);
     })
     .catch(error => {
-        response.status(500).send({
-            message: error.message || "Error al actualizar el alumno"
+      response.status(400).send(error);
+    });
+}
+
+async function updateAlumno(request, response) {
+  const alumnoUpdate = request.body;
+
+  Alumno.update(alumnoUpdate, {
+    where: { ID_Alumno: alumnoUpdate.ID_Alumno }
+  })
+    .then(num => {
+      if (num == 1) {
+        response.status(200).send({
+          message: "Alumno actualizado correctamente"
         });
+      } else {
+        response.status(400).send({
+          message: "No se pudo actualizar el alumno"
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).send({
+        message: error.message || "Error al actualizar el alumno"
+      });
+    });
+}
+
+async function deleteAlumno(request, response) {
+  const id = request.params.id;
+
+  Alumno.destroy({
+    where: { ID_Alumno: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        response.status(200).send({
+          message: "Alumno eliminado correctamente"
+        });
+      } else {
+        response.status(400).send({
+          message: "No se pudo eliminar el alumno"
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).send({
+        message: error.message || "Error al eliminar el alumno"
+      });
     });
 }
 
 module.exports = {
-    findAll,
-    insertAlumno,
-    updateAlumno
+  findAll,
+  insertAlumno,
+  updateAlumno,
+  deleteAlumno
 }
