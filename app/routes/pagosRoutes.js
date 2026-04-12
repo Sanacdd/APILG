5e5e70f2a387575106493e71d892aacd6e173c77
Denis <denisseamador216@gmail.com>
2026-04-12 01:50:54 -0600

    CRUD pagos API con findOne update delete y rutas

--- app/routes/pagosRoutes.js
+++ app/routes/pagosRoutes.js
@@ -5,9 +5,11 @@ const pagosController = require('../Controllers/pagosController');
 const apiRoutes = express.Router();
 
 apiRoutes
-.get('/pagos', async (req, res) => await
-pagosController.findAll(req, res))
-.post('/insertPago', pagosController.insertPago)
-.put('/updatePago', pagosController.updatePago);
+  .get('/pagos', async (req, res) => await pagosController.findAll(req, res))
+  .get('/pagos/:ID_Pagos', pagosController.findOne)
+  .post('/insertPago', pagosController.insertPago)
+  .put('/updatePago', pagosController.updatePago)
+  .delete('/deletePago/:ID_Pagos', pagosController.deletePago);
+  
 
 module.exports = apiRoutes;
