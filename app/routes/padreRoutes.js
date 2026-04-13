73a5792c929fb6512ddd7ea6d8535d94b7677fa5
jimenez-x1 <alejarquitectura@gmail.com>
2026-04-12 22:29:34 -0600

    Api Padre

--- app/routes/padreRoutes.js
+++ app/routes/padreRoutes.js
@@ -1,13 +1,12 @@
 'use strict'
 const express = require('express');
 const padreController = require('../Controllers/padreController'); 
-
 const apiRoutes = express.Router();
 
 apiRoutes
-.get('/padres',async (req, res) => await 
-padreController.findAll(req, res))
+.get('/padres', async (req, res) => await padreController.findAll(req, res))
 .post('/insertPadre', padreController.insertPadre)
-.put('/updatePadre', padreController.updatePadre);
+.put('/updatePadre', padreController.updatePadre)
+.delete('/padres/id', padreController.deletePadre);
 
 module.exports = apiRoutes;
